9de712086fb6abb4b0a402d7fdc7fa370350a8dc
manuelpt4940 <36642986+manuelpt4940@users.noreply.github.com>
2018-06-07 17:13:26 -0500

    Delete redundant Button class code and put code about onBackPressed Button

--- app/src/main/java/com/example/usuario/prueba/MainActivity.java
+++ app/src/main/java/com/example/usuario/prueba/MainActivity.java
@@ -236,10 +236,10 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
+    /*@Override
     public void onBackPressed() {
         Disconnect();
-    }
+    }*/
 
 
 
@@ -290,179 +290,21 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    public class Button extends AppCompatActivity {
 
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            setContentView(R.layout.fragment_finicio);
-
-            Intent newint = getIntent();
-            deviceName = newint.getStringExtra(Devices.EXTRA_NAME);
-            address = newint.getStringExtra(Devices.EXTRA_ADDRESS);
-
-            //TextView statusView = (TextView)findViewById(R.id.status);
-            //Button prender = (Button)findViewById(R.id.prender);
-
-            //final View roundButton = (View)findViewById(R.id.roundButton);
-
-            //statusView.setText("Connecting to " + deviceName);
-
-            new ConnectBT().execute();
-
-           /* roundButton.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        pressed(roundButton, event);
-
-                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                        released(roundButton, event);
-
-                    } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                        moved(roundButton, event);
-                    }
-                    return false;
-                }
-            });*/
-
+    /*@Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }*/
 
-        public void  prenders(View v){
-            send(buildMessage("1",0.5,0.5));
-        }
-        public void  apagars(View v){
-            send(buildMessage("1",0,0));
-        }
-
-
-        private String buildMessage(String operation, double x, double y) {
-            return (operation + "," + String.valueOf(x) + "," + String.valueOf(y) + "\n");
-        }
-
-        private void send(String message) {
-            if (btSocket!=null) {
-                try {
-                    btSocket.getOutputStream().write(message.getBytes());
-                } catch (IOException e) {
-                    //  msg("Error : " + e.getMessage());
-                    if(e.getMessage().contains("Broken pipe")) Disconnect();
-                }
-            } else {
-                //msg("Error : btSocket == null");
-            }
-        }
-
-        private void Disconnect() {
-            if (btSocket!=null) {
-                try {
-                    isBtConnected = false;
-                    btSocket.close();
-                } catch (IOException e) {
-                    //msg("Error");
-                }
-            }
-            Toast.makeText(getApplicationContext(),"Disconnected",Toast.LENGTH_LONG).show();
-            finish();
-        }
-
-        /*private void msg(String message) {
-            TextView statusView = (TextView)findViewById(R.id.status);
-            statusView.setText(message);
-        }*/
-
-        //Clase que se compone de 4 momentos, pre, in, progress y post
-        private class ConnectBT extends AsyncTask<Void, Void, Void> {
-            private boolean ConnectSuccess = true;
-
-            @Override
-            protected void onPreExecute() {
-                progress = ProgressDialog.show(Button.this, "Connecting", "Please wait...");  //show a progress dialog
-            }
-
-            @Override
-            protected Void doInBackground(Void... devices) { //while the progress dialog is shown, the connection is done in background
-                try {
-                    if (btSocket == null || !isBtConnected) {
-                        myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
-                        BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
-                        btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
-                        BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-                        btSocket.connect();//start connection
-                    }
-                } catch (IOException e) {
-                    ConnectSuccess = false;//if the try failed, you can check the exception here
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void result) { //after the doInBackground, it checks if everything went fine
-                super.onPostExecute(result); //Super call the same method or class
-
-                if (!ConnectSuccess) {
-                    Toast.makeText(getApplicationContext(), "Failed to connect", Toast.LENGTH_LONG).show();
-                    finish();
-                } else {
-                    //msg("Connected to " + deviceName);
-                    isBtConnected = true;
-                    // start the connection monitor
-                    new Button.MonitorConnection().execute();
-                }
-                progress.dismiss();
-            }
-        }
-
-        private class MonitorConnection extends AsyncTask<Void, Void, Void> {
-
-            @Override
-            protected Void doInBackground(Void... devices) {
-                while (!connectionLost) {
-                    try {
-                        //read from the buffer, when this errors the connection is lost
-                        // this was the only reliable way I found of monitoring the connection
-                        // .isConnected didnt work
-                        // BluetoothDevice.ACTION_ACL_DISCONNECTED didnt fire
-                        btSocket.getInputStream().read();
-                    } catch (IOException e) {
-                        connectionLost = true;
-                    }
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void result) {
-                super.onPostExecute(result);
-
-                // if the bt is still connected, the connection must have been lost
-                if (isBtConnected) {
-                    try {
-                        isBtConnected = false;
-                        btSocket.close();
-                    } catch (IOException e) {
-                        // nothing doing, we are ending anyway!
-                    }
-                    Toast.makeText(getApplicationContext(), "Connection lost", Toast.LENGTH_LONG).show();
-                    finish();
-                }
-            }
-        }
-        /*@Override
-            public void onBackPressed() {
-                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-                if (drawer.isDrawerOpen(GravityCompat.START)) {
-                    drawer.closeDrawer(GravityCompat.START);
-                } else {
-                    super.onBackPressed();
-                }
-            }*/
-        @Override
-        public void onBackPressed() {
-            Disconnect();
-        }
+    @Override
+    public void onBackPressed() {
+        Disconnect();
     }
+
+
 }
