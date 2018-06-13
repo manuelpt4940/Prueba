c0008111d7e13b5171319874ac52878b05e206e3
manuelpt4940 <36642986+manuelpt4940@users.noreply.github.com>
2018-06-13 12:17:15 -0500

    ProgressBar execute when the app send data to Pi

--- app/src/main/java/com/example/usuario/prueba/FInicio.java
+++ app/src/main/java/com/example/usuario/prueba/FInicio.java
@@ -1,5 +1,7 @@
 package com.example.usuario.prueba;
 
+import android.app.ProgressDialog;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,8 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 
 /*
@@ -19,36 +25,81 @@ import java.io.IOException;
  * Use the {@link FInicio#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FInicio extends Fragment {
-    Button button;
+public class FInicio extends Fragment{
+    Button button, button2;
     TextView tv;
+    private ProgressDialog progressBar;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+                             final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_finicio, container, false);
         // Inflate the layout for this fragment
         button = view.findViewById(R.id.butt);
         tv = view.findViewById(R.id.tv1);
+        button2 = view.findViewById(R.id.button2);
+
+        //progressBar = view.findViewById(R.id.progressBar);
+        progressBar = new ProgressDialog(getActivity());
+
+
         button.setOnClickListener(new View.OnClickListener(){
+              @Override
+              public void onClick(View v) {
+
+                  Enviar("Hola","Enviando1");
+
+              }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
-
-                ((MainActivity)getActivity()).send("Hola"); //Llamar un método del mainActivity
-                ((MainActivity)getActivity()).send("Hola2");
-                try {
-                    ((MainActivity)getActivity()).receive();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String Mess=((MainActivity)getActivity()).readMessage;  //Obtener una variable desde el Activity
-                tv.setText(Mess);
-
-
+                Enviar("Hola2", "Enviando2");
             }
         });
+
+
         return view;
     }
 
+
+
+
+    public void Enviar(final String data, String title) {
+        progressBar = ProgressDialog.show(getActivity(), title, "Please wait...");  //show a progress dialog
+        new Thread(new Runnable() {
+              @Override
+              public void run()
+              {
+                // do the thing that takes a long time
+                    ((MainActivity)getActivity()).send(data);
+
+                    try {
+                        ((MainActivity)getActivity()).receive();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run()
+                        {
+                          String Mess=((MainActivity)getActivity()).readMessage;  //Obtener una variable desde el Activity
+                          tv.setText(Mess);
+                          progressBar.dismiss();
+                        }
+                    });
+              }
+        }).start();
+    }
 }
